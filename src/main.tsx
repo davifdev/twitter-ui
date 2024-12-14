3578de94f16b484cba518760cea963544a4d0c83
Davi Fernandes <davi.fer159@gmail.com>
2024-12-14 18:26:44 -0300

    finish aside init content

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,83 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import {
+  Bell,
+  BookmarkSimple,
+  DotsThreeCircle,
+  Envelope,
+  FileText,
+  Hash,
+  House,
+  Sparkle,
+  User,
+} from "phosphor-react";
 
 import "./global.css";
 
-import Tweet from "./components/Tweet";
+import twitterLogo from "./assets/logo-twitter.svg";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <div>
-      <Tweet user="Davi" likes={15}>
-        Meu primeiro tweet
-      </Tweet>
-      <Tweet user="Laura" likes={20}>
-        Hello World!
-      </Tweet>
-      <Tweet user="Roberval">Olá, Mundo!</Tweet>
+    <div className="layout">
+      <aside className="sidebar">
+        <img className="logo" src={twitterLogo} alt="foto" />
+
+        <nav className="main-navigation">
+          <a className="active" href="#">
+            <House weight="fill" />
+            Home
+          </a>
+          <a href="#">
+            <Hash />
+            Explore
+          </a>
+          <a href="#">
+            <Bell />
+            Notifications
+          </a>
+          <a href="#">
+            <Envelope />
+            Messages
+          </a>
+          <a href="#">
+            <BookmarkSimple />
+            Bookmarks
+          </a>
+          <a href="#">
+            <FileText />
+            List
+          </a>
+          <a href="#">
+            <User />
+            Profile
+          </a>
+          <a href="#">
+            <DotsThreeCircle />
+            More
+          </a>
+        </nav>
+
+        <button className="new-tweet" type="button">
+          Tweet
+        </button>
+      </aside>
+      <div className="content">
+        <main className="timeline">
+          <div className="timeline-header">
+            Home
+            <Sparkle />
+          </div>
+
+          <form className="new-tweet-form">
+            <label htmlFor="tweet">
+              <img src="https://github.com/davifdev.png" alt="Davi Fernandes" />
+              <textarea placeholder="What´s happening?" />
+            </label>
+
+            <button type="submit">Tweet</button>
+          </form>
+        </main>
+      </div>
     </div>
   </StrictMode>
 );
